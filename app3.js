807d4b309baf8bb2e4bb27317f792e1c9d433da9
MirzaAyon <ayonmirza98@gmail.com>
2022-06-19 09:59:04 +0600

    app3 file added where full process has done by onclick method

---
+++ app3.js
@@ -0,0 +1,20 @@
+let count = 0;
+
+// document.getElementById('add').addEventListener('click', ); 
+//click er aag porjnto kaj,onclick e kore dibe
+function add(e) {
+    count++;
+    control('count');
+}
+
+function minus(e) {
+    count--;
+    control('count');
+}
+
+
+
+function control(id) {
+    document.getElementById(id).innerText = count;
+    // console.log(id);
+}
